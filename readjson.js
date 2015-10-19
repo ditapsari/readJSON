86284b0abfd665cbd85c8958f861897abc42c08b
Dita praditya Sari <ditapraditya1@gmail.com>
2015-10-19 19:04:13 +0700

    Mengupdate file

--- readjson.js
+++ readjson.js
@@ -2,8 +2,12 @@ var fs = require('fs');
 
 fs.readFile(__dirname + '/read.json', function(err, data) {
     var jsonObj = JSON.parse(data);
-    console.log("Name = " + jsonObj.firstName + " " + jsonObj.lastName);
-    console.log("Born = " + jsonObj.born);
-    console.log("Address" +jsonObj.address);
-
-});
+    
+    var jumrec = jsonObj.length;
+    for(var x=0;x<jumrec;x++){
+        console.log("Name = " + jsonObj.firstName + " " + jsonObj.lastName);
+        console.log("Born = " + jsonObj.born);
+        console.log("Address" +jsonObj.address);
+    }
+    
+    });
